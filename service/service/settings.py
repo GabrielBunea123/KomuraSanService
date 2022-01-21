26b942534585e4e0a21c5807c6966c38ae0aac63
GabrielBunea123 <76706013+GabrielBunea123@users.noreply.github.com>
2022-01-21 12:34:48 +0200

    staticfiles

--- service/service/settings.py
+++ service/service/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '9xfe$yy^ygn-olg=ks(evli$f(q2uz4f+^al%&=ld!0*j32j$f'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -122,6 +122,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'bookhouse356@gmail.com'
@@ -130,3 +136,7 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+import django_heroku
+
+django_heroku.settings(locals())
