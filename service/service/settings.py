e5e01f70da345c49458babf2666ef1d7760449c0
GabrielBunea123 <76706013+GabrielBunea123@users.noreply.github.com>
2022-04-03 11:32:33 +0300

    email changes

--- service/service/settings.py
+++ service/service/settings.py
@@ -137,8 +137,8 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'bookhouse356@gmail.com'
-EMAIL_HOST_PASSWORD = 'haxasxagsbroetib'
+EMAIL_HOST_USER = 'komurasanservice@gmail.com'
+EMAIL_HOST_PASSWORD = 'fjnvpnfctxamvyat'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
